4cc711ef0e5b0ca97844fe797c22e99de4ea2876
NiwradMendyke <mendyke@usc.edu>
2016-02-12 22:30:06 -0800

    uploading

---
+++ hw3/test/main.cpp
@@ -0,0 +1,50 @@
+#include "../llistint.h"
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+using namespace std;
+
+void output(LListInt &list) {
+	for (int i = 0; i < list.size(); i++)
+		cout << list[i] << " ";
+	cout << endl;
+}
+
+int main() {
+	cout << "main function" << endl;
+	LListInt *list1 = new LListInt();
+	cout << "new LListInt" << endl;
+
+	list1->insert(0, 5);
+	list1->insert(0, 4);
+	list1->insert(0, 1);
+	list1->insert(0, 2);
+	list1->insert(0, 2);
+
+	LListInt *list2 = new LListInt();
+	cout << "new LListInt" << endl;
+	list2->insert(0, 3);
+	list2->insert(0, 1);
+
+	LListInt *list3 = new LListInt();
+	cout << "new LListInt" << endl;
+	list3->insert(0, 3);
+	list3->insert(0, 3);
+	list3->insert(0, 3);
+
+	output(*list2);
+
+	//*list2 = *list1;
+
+	delete list1;
+
+	output(*list2);
+
+	//*list2 = ((*list2) + (*list3));
+
+	delete list2;
+	delete list3;
+
+	return 0;
+}
